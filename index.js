8faeb370a75e36b3d3b1456c3aae6f33b536d539
Nelli Butenko <butenkonelly93@gmail.com>
2021-06-05 16:36:13 +0000

    Install socket io and establish connection

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+// initializes app to be a function handler that you can supply to an HTTP server (line 4)
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+// run "npm install socket.io", it added dependencies to pckg.json, add it to index now
+// initialize a new instance of socket.io by passing the SERVER (http server) object
+const { Server } = require("socket.io");
+const io = new Server(server);
 
+// define a route handler / that gets called when we hit our website home
+// how does "__dirname + " work?
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
+// listen on 'connection' event for incoming sockets and log it into console
+io.on('connection', (socket) => {
+  // connection event fires and logs in terminal when open or refresh the page in browser
+  console.log('a user connected');
+  // disconnection event fires when user closes the page in browser
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+// make the http server listen on port 3000
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
